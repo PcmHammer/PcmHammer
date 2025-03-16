ac231aa3f2bd5c93fb67a63bc895c6df6766837e
NSFW <nsfw@outlook.com>
2025-03-15 17:10:37 -0700

    Logging back-end written, no UI yet.

--- Apps/UI/UnoUI/PcmHacking.UnoUI/Presentation/DataLoggingParametersModel.cs
+++ Apps/UI/UnoUI/PcmHacking.UnoUI/Presentation/DataLoggingParametersModel.cs
@@ -1,7 +1,14 @@
+using System.Collections.Concurrent;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Microsoft.Extensions.Logging;
 using Microsoft.UI.Dispatching;
 using PcmHacking.UnoUI.Services;
+using Uno.Extensions;
 
 namespace PcmHacking.UnoUI.Presentation;
+
+public record LogRowValues(IEnumerable<string> Values);
 
 public partial record DataLoggingParametersModel
 {
@@ -10,8 +17,11 @@ public partial record DataLoggingParametersModel
     private ISettingsService settingsService;
     private PcmHacking.ILogger progressLogger;
     private string profilePath;
-    //private string canPortName;
+    private string canPortName;
     private CanLogger? canLogger;
+    private ConcurrentQueue<Tuple<Logger, LogFileWriter?, IEnumerable<string>>> logRowQueue = new ConcurrentQueue<Tuple<Logger, LogFileWriter?, IEnumerable<string>>>();
+    private EventWaitHandle exitWaitHandle = new EventWaitHandle(false, EventResetMode.ManualReset);
+    private EventWaitHandle rowAvailableHandle = new EventWaitHandle(false, EventResetMode.ManualReset);
 
     public DataLoggingParametersModel(
         INavigator navigator,
@@ -26,7 +36,7 @@ public partial record DataLoggingParametersModel
         this.settingsService = settingsService;
         this.progressLogger = progressLogger;
         this.profilePath = profilePath;
-        //this.canPortName = settingsService.GetCanSerialPortName(CancellationToken.None).Result;
+        this.canPortName = settingsService.GetCanSerialPortName();
 
         dispatcherQueue.TryEnqueue(async () =>
         {
@@ -35,53 +45,89 @@ public partial record DataLoggingParametersModel
         });
     }
 
+    public IState<LogProfile> LogProfile => State<LogProfile>.Empty(this);
+    public IFeed<LogRowValues> Rows => Feed<LogRowValues>.AsyncEnumerable(this.RowFactory);
+
     private async Task OpenProfile()
     {
-        var lease = await this.connectionService.BeginActivity("Logging", true);
-        if (lease == null)
+        using (var lease = await this.connectionService.BeginActivity("Logging", true))
         {
-            this.progressLogger.AddUserMessage("No vehicle connected.");
-            return;
+            if (lease == null)
+            {
+                this.progressLogger.AddUserMessage("No vehicle connected.");
+                return;
+            }
+
+            var vehicle = lease.Vehicle;
+
+            // Load the database
+            string appDirectory = AppContext.BaseDirectory;
+            var database = new ParameterDatabase(appDirectory);
+            database.LoadDatabase();
+
+            // Create the log profile
+            var osidQueryResult = await vehicle.QueryOperatingSystemId(CancellationToken.None);
+            uint osid = osidQueryResult.Value;
+            LogProfileReader reader = new LogProfileReader(database, osid, this.progressLogger);
+            var profile = reader.Read(this.profilePath);
+
+            // This tells the view to update the UI with the new profile.
+            await this.LogProfile.UpdateAsync(currentValue => profile, CancellationToken.None);
+
+            // Create the logger, and start logging.
+            this.canLogger = new CanLogger(database);
+
+            if (string.IsNullOrEmpty(this.canPortName))
+            {
+                await this.canLogger.SetPort(null);
+            }
+            else
+            {
+                await this.canLogger.SetPort(new StandardPort(canPortName));
+
+            }
+
+            Logger logger = vehicle.CreateLogger(osid, canLogger, profile.Columns, this.progressLogger);
+            while (!exitWaitHandle.WaitOne(0))
+            {
+                IEnumerable<string> rowValues = await logger.GetNextRow();
+                if (rowValues != null)
+                {
+                    // Hand this data off to be written to disk and displayed in the UI.
+                    this.logRowQueue.Enqueue(
+                        new Tuple<Logger, LogFileWriter?, IEnumerable<string>>(
+                            logger,
+                            null, // file writer
+                            rowValues));
+
+                    this.rowAvailableHandle.Set();
+                }
+            }
         }
-
-        var vehicle = lease.Vehicle;
-
-        // Load the database
-        string appDirectory = AppContext.BaseDirectory;
-        var database = new ParameterDatabase(appDirectory);
-        database.LoadDatabase();
-
-        // Create the log profile
-        var osidQueryResult = await vehicle.QueryOperatingSystemId(CancellationToken.None);
-        uint osid = osidQueryResult.Value;
-        LogProfileReader reader = new LogProfileReader(database, osid, this.progressLogger);
-        var profile = reader.Read(this.profilePath);
-        this.populateParameterGrid(profile);
-
-        // Create the logger
-        this.canLogger = new CanLogger(database);
-
-        /*
-        if (string.IsNullOrEmpty(this.canPortName))
-        {
-            await this.canLogger.SetPort(null);
-        }
-        else
-        {
-            await this.canLogger.SetPort(new StandardPort(canPortName));
-
-        }
-
-        Logger logger = this.Vehicle.CreateLogger(this.osid, canLogger, this.currentProfile.Columns, this);
-        */
-
     }
 
-    private void populateParameterGrid(LogProfile profile)
+    private async IAsyncEnumerable<LogRowValues> RowFactory([EnumeratorCancellation] CancellationToken ct)
     {
-        //foreach (LogColumn column in this.profile.Columns)
+        WaitHandle[] waitHandles = new WaitHandle[] { exitWaitHandle, ct.WaitHandle, rowAvailableHandle };
+
+        while (true)
         {
-            //       this.Param
+            int index = await Task.Run(() => WaitHandle.WaitAny(waitHandles));
+            if (index == 0 || index == 1)
+            {
+                break;
+            }
+
+            if (logRowQueue.TryDequeue(out var row))
+            {
+                var (logger, logFileWriter, logRowValues) = row;
+                if (logFileWriter != null)
+                {
+                    logFileWriter.WriteLine(logRowValues);
+                }
+
+                yield return new LogRowValues(logRowValues);
+            }
         }
     }
 }
