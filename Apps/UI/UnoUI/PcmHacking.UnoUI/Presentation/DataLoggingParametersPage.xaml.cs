7ec70bcce57d570ce6c926d9566c947cc442b258
NSFW <nsfw@outlook.com>
2025-03-15 17:30:48 -0700

    Added log front end, not tested yet.

--- Apps/UI/UnoUI/PcmHacking.UnoUI/Presentation/DataLoggingParametersPage.xaml.cs
+++ Apps/UI/UnoUI/PcmHacking.UnoUI/Presentation/DataLoggingParametersPage.xaml.cs
@@ -10,7 +10,7 @@ public sealed partial class DataLoggingParametersPage : Page
     {
         this.InitializeComponent();
 
-        this.DataContextChanged += (sender, e) =>
+        this.DataContextChanged += async (sender, e) =>
         {
             var newModel = (this.DataContext as DataLoggingParametersViewModel)?.Model as DataLoggingParametersModel;
             if (newModel == null)
@@ -25,18 +25,72 @@ public sealed partial class DataLoggingParametersPage : Page
 
             this.model = newModel;
 
-            this.InitializeParameters();
+            await this.model.LogProfile.Select((profile) =>
+            {
+                this.InitializeParameters(profile);
+                return true;
+            });
+
+            await this.model.Rows.Select((row) =>
+            {
+                this.UpdateParameterValues(row.Values);
+                return true;
+            });
         };
     }
 
-    private void InitializeParameters()
+    private List<TextBlock> parameterValues;
+
+    private void InitializeParameters(LogProfile profile)
     {
         if (this.model == null)
         {
             return;
         }
 
-        
+        int row = 0;
+        foreach(var column in profile.Columns)
+        {
+            TextBlock name = new TextBlock();
+            name.Text = column.Name;
+            name.Margin = new Thickness(5);
+            name.HorizontalAlignment = HorizontalAlignment.Left;
+            name.VerticalAlignment = VerticalAlignment.Center;
+            name.SetValue(Grid.RowProperty, row);
+            name.SetValue(Grid.ColumnProperty, 0);
+
+            TextBlock value = new TextBlock();
+            value.Text = column.Value.ToString();
+            value.Margin = new Thickness(5);
+            value.HorizontalAlignment = HorizontalAlignment.Left;
+            value.VerticalAlignment = VerticalAlignment.Center;
+            value.SetValue(Grid.RowProperty, row);
+            value.SetValue(Grid.ColumnProperty, 1);
+            row++;
+            parameterValues.Add(value);
+            this.Parameters.Children.Add(name);
+            this.Parameters.Children.Add(value);
+        }
+    }
+
+    private void UpdateParameterValues(IEnumerable<string> newValues)
+    {
+        if (this.model == null)
+        {
+            return;
+        }
+
+        int row = 0;
+        foreach (var value in newValues)
+        {
+            if (row >= this.parameterValues.Count)
+            {
+                break;
+            }
+            var textBlock = this.parameterValues[row];
+            textBlock.Text = value;
+            row++;
+        }
     }
 }
 
