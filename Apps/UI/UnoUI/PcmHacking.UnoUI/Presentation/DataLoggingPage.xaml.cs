6ab5be158392e72317495c555ac47c5b435259fd
NSFW <nsfw@outlook.com>
2025-05-03 13:29:15 -0700

    Fixed double-tap for data logging recent-file list.

--- Apps/UI/UnoUI/PcmHacking.UnoUI/Presentation/DataLoggingPage.xaml.cs
+++ Apps/UI/UnoUI/PcmHacking.UnoUI/Presentation/DataLoggingPage.xaml.cs
@@ -1,15 +1,19 @@
 using Microsoft.UI.Xaml.Input;
+using Windows.Foundation;
 
 namespace PcmHacking.UnoUI.Presentation;
 
 public sealed partial class DataLoggingPage : Page
 {
+    private int lastTapTime = 0;
+    private Point lastTapPoint = new Point(0, 0);
+
     public DataLoggingPage()
     {
         this.InitializeComponent();
     }
 
-    private async void ListView_DoubleTapped(object sender, Microsoft.UI.Xaml.Input.DoubleTappedRoutedEventArgs e)
+    private async void ListView_DoubleTapped(object sender, Microsoft.UI.Xaml.Input.DoubleTappedRoutedEventArgs? e)
     {
         RecentFileListItem? recentFile = this.RecentFiles.SelectedItem as RecentFileListItem;
         if (recentFile == null)
@@ -42,6 +46,33 @@ public sealed partial class DataLoggingPage : Page
 
         Task task = (this.DataContext as DataLoggingViewModel)?.Model?.RecentProfileClicked(recentFile) ?? Task.CompletedTask;
         await task;
+    }
+
+    private void RecentFiles_Tapped(object sender, TappedRoutedEventArgs e)
+    {
+        int currentTime = Environment.TickCount;
+        Point currentPoint = e.GetPosition(this.RecentFiles);
+        try
+        {
+            if (currentTime - lastTapTime > 250)
+            {
+                return;
+            }
+
+            const int tapRegionSize = 15;
+            if (Math.Abs(currentPoint.X - lastTapPoint.X) > tapRegionSize || 
+                Math.Abs(currentPoint.Y - lastTapPoint.Y) > tapRegionSize)
+            {
+                return;
+            }
+
+            this.ListView_DoubleTapped(sender, null);
+        }
+        finally
+        {
+            this.lastTapTime = currentTime;
+            this.lastTapPoint = currentPoint;
+        }
     }
 }
 
