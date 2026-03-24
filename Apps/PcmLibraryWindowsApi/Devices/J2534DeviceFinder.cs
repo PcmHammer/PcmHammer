e69179a3ceab5ed310a31d10198b27b7fc705466
Crowbarmaster <Crowbarmasterhl2@gmail.com>
2026-03-24 00:35:15 -0400

    In my driver list seemingly exists an entry with a null/empty name. This caused havoc with the UI, always defaulting to that selection as active. Filter out any empty entries.
    TODO? Perhaps we could rename the device to "Unnamed device" and add it, in the event it's some glitch with a driver. Your call good sir.

--- Apps/PcmLibraryWindowsApi/Devices/J2534DeviceFinder.cs
+++ Apps/PcmLibraryWindowsApi/Devices/J2534DeviceFinder.cs
@@ -18,8 +18,8 @@ namespace PcmHacking
             List<J2534DeviceInfo> installedDLLs = new List<J2534DeviceInfo>();
             try
             {
-                installedDLLs = J2534DotNet.J2534Detect.ListDevices();
-                return installedDLLs;
+                installedDLLs = J2534Detect.ListDevices();
+                return [.. installedDLLs.Where(x => !string.IsNullOrWhiteSpace(x.Name))];
             }
             catch (Exception exception)
             {
