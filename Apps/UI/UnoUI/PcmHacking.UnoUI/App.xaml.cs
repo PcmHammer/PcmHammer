60374bf25cc2bdc41ef060cb3f026e8db042122d
Crowbarmaster <Crowbarmasterhl2@gmail.com>
2026-04-09 23:53:08 -0400

    Added CancellationTokenSource as means to help close the device moments before closure.
    Added the means to log additional stacktrace information from an Unhandled Exception.

--- Apps/UI/UnoUI/PcmHacking.UnoUI/App.xaml.cs
+++ Apps/UI/UnoUI/PcmHacking.UnoUI/App.xaml.cs
@@ -1,7 +1,9 @@
 using CommunityToolkit.Mvvm.Messaging;
 using Microsoft.UI.Dispatching;
 using PcmHacking.UnoUI.Services;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.Text;
 using Uno.Resizetizer;
 
 namespace PcmHacking.UnoUI;
@@ -15,8 +17,12 @@ public partial class App : Application
     /// See SettingsModel.OpenLogFolderPicker for example.
     /// </remarks>
     public static Window? StaticMainWindow { get; private set; }
+
+    public static CancellationTokenSource ApplicationShutdownSource = new();
         
     private DispatcherQueue dispatcherQueue = DispatcherQueue.GetForCurrentThread();
+
+    private static int _unhandledExceptionExchange;
 
     /// <summary>
     /// Initializes the singleton application object. This is the first line of authored code
@@ -26,6 +32,42 @@ public partial class App : Application
     {
         App.InitializeLogger();
         this.InitializeComponent();
+
+        AppDomain.CurrentDomain.UnhandledException += (sender, eventArgs) => {
+            if (Interlocked.CompareExchange(ref _unhandledExceptionExchange, 1, 0) == 0)
+            {
+                try
+                {
+                    StackTrace? currentStackTrace;
+
+                    try
+                    {
+                        currentStackTrace = new StackTrace(1, true);
+                    }
+                    catch
+                    {
+                        currentStackTrace = null;
+                    }
+
+                    Debug.WriteLine(new StringBuilder()
+                        .AppendLine($"{DateTime.Now:O} DCA exception thrown: {((Exception)eventArgs.ExceptionObject).Message}")
+                        .AppendLine("----- Exception -----")
+                        .AppendLine(((Exception)eventArgs.ExceptionObject).ToString().TrimEnd())
+                        .AppendLine("----- Full Stack -----")
+                        .AppendLine(currentStackTrace?.ToString().TrimEnd())
+                        .AppendLine()
+                        .ToString());
+                }
+                catch
+                {
+                    // ignored
+                }
+                finally
+                {
+                    Interlocked.Exchange(ref _unhandledExceptionExchange, 0);
+                }
+            }
+        };
     }
 
     protected Window? MainWindow { get; private set; }
@@ -181,6 +223,10 @@ public partial class App : Application
             );
         MainWindow = builder.Window;
         StaticMainWindow = builder.Window;
+        MainWindow.AppWindow.Closing += (s, e) =>
+        {
+            App.ApplicationShutdownSource.Cancel();
+        };
 
 #if WINDOWS
         StaticMainWindow.Title = "PCM Hammer";
