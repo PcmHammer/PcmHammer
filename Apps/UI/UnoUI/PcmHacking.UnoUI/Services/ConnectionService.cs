9b5c5118a14354ba6eef0107fe9a34f8a5da63b7
Crowbarmaster <Crowbarmasterhl2@gmail.com>
2026-04-10 01:43:01 -0400

    Moved final description to label already connected devices.

--- Apps/UI/UnoUI/PcmHacking.UnoUI/Services/ConnectionService.cs
+++ Apps/UI/UnoUI/PcmHacking.UnoUI/Services/ConnectionService.cs
@@ -286,9 +286,9 @@ public class ConnectionService : IConnectionService
         }
 
         Device? newDevice = null;
+        string portDesc = settings.DeviceCategory == DeviceConfiguration.Constants.DeviceCategorySerial ? settings.DeviceNameOrPort : settings.DeviceCategory;
         if (this.device == null || settings != this.newSettings)
         {
-            string portDesc = settings.DeviceCategory == DeviceConfiguration.Constants.DeviceCategorySerial ? settings.DeviceNameOrPort : settings.DeviceCategory;
             await this.DeviceName.SetAsync($"Detecting({portDesc})");
             await this.DeviceState.SetAsync("Connecting...");
 
@@ -301,10 +301,6 @@ public class ConnectionService : IConnectionService
                 else
                 {
                     newDevice = DeviceFactory.CreateDevice(this.logger, settings.DeviceCategory, settings.DeviceNameOrPort);
-                }
-                if (newDevice != null)
-                {
-                    await this.DeviceName.SetAsync($"{newDevice.GetDeviceType()}({portDesc})");
                 }
             }
             catch (TimeoutException)
@@ -324,7 +320,7 @@ public class ConnectionService : IConnectionService
         {
             return (null, null);
         }
-
+        await this.DeviceName.SetAsync($"{newDevice.GetDeviceType()}({portDesc})");
         ToolPresentNotifier notifier = new ToolPresentNotifier(this.device, this.protocol, this.logger);
         string basePath = string.Empty; // We will need to pass along a path to target kernel; Android won't path to a proper directory with GetExecutingAssembly().Location.
 #if WINDOWS
