e353f69b47d33f27adcb2e042bd31512a8a64c0a
Crowbarmaster <Crowbarmasterhl2@gmail.com>
2026-04-13 02:39:05 -0400

    Add DataService that can be called to run a task in the background. All supporting logic included.
    Refactor ReadModel to support background opperations.

--- Apps/UI/UnoUI/PcmHacking.UnoUI/Platforms/Android/PermissionMethods.Android.cs
+++ Apps/UI/UnoUI/PcmHacking.UnoUI/Platforms/Android/PermissionMethods.Android.cs
@@ -9,6 +9,7 @@ namespace PcmHacking.UnoUI.Platforms.Android
     {
         public static async Task RequestAndroidPermissions()
         {
+            await Windows.Extensions.PermissionsHelper.TryGetPermission(CancellationToken.None, Manifest.Permission.PostNotifications);
             await Windows.Extensions.PermissionsHelper.TryGetPermission(CancellationToken.None, Manifest.Permission.Bluetooth);
             await Windows.Extensions.PermissionsHelper.TryGetPermission(CancellationToken.None, Manifest.Permission.BluetoothAdvertise);
             await Windows.Extensions.PermissionsHelper.TryGetPermission(CancellationToken.None, Manifest.Permission.BluetoothScan);
@@ -21,6 +22,8 @@ namespace PcmHacking.UnoUI.Platforms.Android
             await Windows.Extensions.PermissionsHelper.TryGetPermission(CancellationToken.None, Manifest.Permission.AccessBackgroundLocation);
             await Windows.Extensions.PermissionsHelper.TryGetPermission(CancellationToken.None, Manifest.Permission.AccessFineLocation);
             await Windows.Extensions.PermissionsHelper.TryGetPermission(CancellationToken.None, Manifest.Permission.LocationHardware);
+            await Windows.Extensions.PermissionsHelper.TryGetPermission(CancellationToken.None, Manifest.Permission.ForegroundService);
+            await Windows.Extensions.PermissionsHelper.TryGetPermission(CancellationToken.None, Manifest.Permission.ForegroundServiceDataSync);
             if (global::Android.OS.Build.VERSION.SdkInt >= global::Android.OS.BuildVersionCodes.R)
             {
                 bool result = global::Android.OS.Environment.IsExternalStorageManager;
