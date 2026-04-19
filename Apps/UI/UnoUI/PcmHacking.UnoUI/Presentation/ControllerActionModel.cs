88c156e99922a117a2a1be8c3432f720b9b7990d
Crowbarmaster <Crowbarmasterhl2@gmail.com>
2026-04-19 00:27:08 -0400

    ReadPage/Model retooled to become a simple output page to view and cancel the progress of the requested action.

--- Apps/UI/UnoUI/PcmHacking.UnoUI/Presentation/ControllerActionModel.cs
+++ Apps/UI/UnoUI/PcmHacking.UnoUI/Presentation/ControllerActionModel.cs
@@ -5,18 +5,24 @@ using System.Runtime.CompilerServices;
 using Uno.Extensions.Reactive.Commands;
 using Windows.Devices.Bluetooth.Advertisement;
 using Windows.Storage.Pickers;
+using Windows.UI.Core;
 
 namespace PcmHacking.UnoUI.Presentation;
 
-public partial record ReadModel : IAsyncLogger
+public partial record ControllerActionModel : IAsyncLogger
 {
+    public static ECUActionArguments? ECUActionArguments = null;
+    public static bool ShouldNavigateToFunctions = false;
+
     private readonly INavigator navigator;
     private readonly IConnectionService connectionService;
     private readonly ISettingsService settingsService;
     private readonly LoggerAdapter loggerAdapter;
     private readonly IPlatformService platformService;
     private readonly IDispatcher dispatcher;
-    private StorageFile _selectedFile;
+    private StorageFile? _selectedFile;
+    private ControllerPageObjects pageObjects;
+    private readonly string actionText;
 
     private CancellationTokenSource? tokenSource;
     const string defaultPath = "No file selected.";
@@ -26,8 +32,8 @@ public partial record ReadModel : IAsyncLogger
     public IState<bool> StartEnabled => State<bool>.Value(this, () => true);
     public IState<bool> CancelEnabled => State<bool>.Value(this, () => false);
 
-    public IState<string> Path => State<string>.Value(this, () => defaultPath);
-    public IState<string> UserLog => State<string>.Value(this, () => String.Empty);
+    public IState<string> Path => State<string>.Value(this, () => defaultPath); 
+    public IListState<string> UserLog => ListState<string>.Empty(this);
     public IState<string> Activity => State<string>.Value(this, () => String.Empty);
     public IState<string> TimeRemaining => State<string>.Value(this, () => String.Empty);
     public IState<string> PercentDone => State<string>.Value(this, () => String.Empty);
@@ -39,9 +45,11 @@ public partial record ReadModel : IAsyncLogger
     public IState<bool> UseCustomKeyEnabled => State<bool>.Value(this, () => true);
     public IState<string> CustomKey => State<string>.Value(this, () => "");
     public IState<bool> CustomKeyEnabled => State<bool>.Value(this, () => true);
+    public IState<string> CancelText => State<string>.Value(this, ()=> "Cancel");
     private List<string> _localUserMessages;
+    private bool _isActive = false;
 
-    public ReadModel(
+    public ControllerActionModel(
         INavigator navigator,
         IConnectionService connectionService,
         ISettingsService settingsService,
@@ -56,10 +64,22 @@ public partial record ReadModel : IAsyncLogger
         this.platformService = platformService ?? throw new ArgumentNullException(nameof(platformService));
         this.dispatcher = dispatcher ?? throw new ArgumentNullException(nameof(dispatcher));
         
-        var _1 = this.UseCustomKey.SetAsync(this.settingsService.GetUseCustomKey());
-        var _2 = this.CustomKey.SetAsync(this.settingsService.GetCustomKey());
-        var _3 = this.EnableControls(false);
+        _ = this.UseCustomKey.SetAsync(this.settingsService.GetUseCustomKey());
+        _ = this.CustomKey.SetAsync(this.settingsService.GetCustomKey());
+        _ = this.EnableControls(false);
         _localUserMessages = [];
+        pageObjects = new ControllerPageObjects
+        {
+            PromptYesOrNo = this.PromptForYesNo,
+            Invoke = this.Invoke,
+            ShowAlert = this.Alert,
+            PromptForHardwareType = null,
+        };
+        this.actionText = $"{(ECUActionArguments.SelectedAction == ControllerActions.Write ? $"{ECUActionArguments.WriteType} " : "")}{ECUActionArguments.SelectedAction}";
+        if(ECUActionArguments != null)
+        {
+            _ = Start();
+        }
     }
 
     private async ValueTask UseCustomKeyChanged(bool value, CancellationToken ct)
@@ -83,30 +103,15 @@ public partial record ReadModel : IAsyncLogger
         await this.CancelEnabled.SetAsync(busy);
     }
 
-    [Command]
-    public async ValueTask Start(CancellationToken cancellationToken)
+    public async ValueTask Start()
     {
+        _isActive = true;
+        ShouldNavigateToFunctions = true;
+        await this.AddUserMessage($"Beginning selected {this.actionText} operation.");
 #if ANDROID
-        await Platforms.Android.PermissionMethods.ExtractKernelsToFileAndroid(); // Approach with a fire-and-forget tactic - Should complete well before an action will run.
+        await Platforms.Android.PermissionMethods.ExtractKernelsToFileAndroid();
 #endif
         await this.EnableControls(true);
-
-        string? path = string.Empty;
-#if !ANDROID
-        path = await this.Path.Value();
-#endif
-        if (string.IsNullOrWhiteSpace(path) || string.Compare(path, defaultPath, StringComparison.OrdinalIgnoreCase) == 0)
-        {
-            path = await this.PromptForFileSavePath();
-            if (string.IsNullOrWhiteSpace(path) || string.Compare(path, defaultPath, StringComparison.OrdinalIgnoreCase) == 0)
-            {
-                await this.AddUserMessage("No file selected.");
-                await this.StartEnabled.SetAsync(true);
-                await this.CancelEnabled.SetAsync(false);
-                return;
-            }
-            await this.Path.SetAsync(path);
-        }
 
         // TODO: Review the scenarios in which the given cancellationToken
         // can get signaled, and review how the read process handles those
@@ -115,83 +120,63 @@ public partial record ReadModel : IAsyncLogger
         CancellationToken readCancellationToken = this.tokenSource.Token;
         try
         {
-            ConnectionLease lease = await this.connectionService.BeginActivity("Reading PCM", false);
-            using (new LogInterceptor(this.loggerAdapter, this))
+            ConnectionLease lease = await this.connectionService.BeginActivity($"{this.actionText} PCM", false);
+            LogInterceptor interceptor = new LogInterceptor(this.loggerAdapter, this);
+
+            lease.Vehicle.Enable4xReadWrite = ECUActionArguments?.UseHighSpeed ?? false;
+            lease.Vehicle.UserDefinedKey = (ECUActionArguments?.CustomKey ?? 0) == 0 ? -1 : (int)(ECUActionArguments?.CustomKey ?? 0);
+
+            Progress<ProgressUpdate>? progress = new Progress<ProgressUpdate>((progress) =>
             {
-                // I suspect a bug in the Uno Platform's ContentDialog implementation, hence the static object in the 'if' statement.
-                // See notes in WriteModel for details.
-                await this.navigator.GetDataAsync<DelayModel, DelayResult>(this, cancellation: cancellationToken);
-                if (DelayModel.Result?.Proceed == false)
-                {
-                    await this.AddUserMessage("Read aborted.");
-                    return;
-                }
+                _ = UpdateProgress(progress);
+            });
 
-                lease.Vehicle.Enable4xReadWrite = this.settingsService.Is4xReadWriteEnabled();
+            ControllerManager manager = new(lease.Vehicle,
+                ECUActionArguments ?? new(),
+                pageObjects,
+                readCancellationToken,
+                progress,
+                this.loggerAdapter);
 
-                string customKeyString = await this.CustomKey.Value() ?? String.Empty;
-                uint customKey;
-                if (UInt32.TryParse(customKeyString,
-                    System.Globalization.NumberStyles.HexNumber,
-                    CultureInfo.InvariantCulture,
-                    out customKey) && await this.UseCustomKey.Value())
-                {
-                    lease.Vehicle.UserDefinedKey = (int)customKey;
-                }
-                else
-                {
-                    lease.Vehicle.UserDefinedKey = -1;
-                }
-
-                ReadManager readManager = new(
-                    this.loggerAdapter,
-                    lease.Vehicle,
-                    this.Invoke,
-                    this.PromptForFileSavePath,
-                    this.PromptForOperatingSystemId,
-                    this.Alert,
-                    this.PromptForYesNo,
-                    readCancellationToken);
+            manager.Initialize();
 #if WINDOWS
                 using (new AwayMode())
                 {
-                    await performRead(path, lease, readManager);
+                    await PerformControllerAction(manager);
                     lease.Dispose();
+                    interceptor.Dispose();
                 }
 #elif ANDROID
-                Progress<ProgressUpdate> progress = new Progress<ProgressUpdate>((progress) => {
-                    _ = UpdateProgress(progress);
+            Platforms.Android.DataService.StartService(this.actionText, PerformControllerAction(manager),
+                async () =>
+                {
+                    if (readCancellationToken.IsCancellationRequested)
+                    {
+                        await this.AddUserMessage($"{this.actionText} was canceled.");
+                    }
+                    else
+                    {
+                        await this.AddUserMessage($"{this.actionText} completed successfully.");
+                    }
+                    this.tokenSource = null;
+                    await this.EnableControls(false);
+                    lease.Dispose();
+                    interceptor.Dispose();
+                },
+                async () =>
+                {
+                    await this.AddUserMessage($"{this.actionText} failed: ");
+                    this.tokenSource = null;
+                    await this.EnableControls(false);
+                    lease.Dispose();
+                    interceptor.Dispose();
                 });
-
-                Platforms.Android.DataService.StartService("Read PCM", performRead(path, lease, readManager, progress),
-                    async () =>
-                    {
-                        if (readCancellationToken.IsCancellationRequested)
-                        {
-                            await this.AddUserMessage("Read was canceled.");
-                        }
-                        else
-                        {
-                            await this.AddUserMessage("Read completed successfully.");
-                        }
-                        this.tokenSource = null;
-                        await this.EnableControls(false);
-                        lease.Dispose();
-                    },
-                    async () =>
-                    {
-                        await this.AddUserMessage("Read failed: ");
-                        this.tokenSource = null;
-                        await this.EnableControls(false);
-                        lease.Dispose();
-                    });
-                return;
+            return;
 #endif
-            }
         }
         catch (Exception exception)
         {
-            await this.AddUserMessage("Read failed: ");
+            await this.AddUserMessage($"{this.actionText} failed: ");
             await this.AddUserMessage(exception.Message);
             await this.AddDebugMessage(exception.ToString());
         }
@@ -201,15 +186,61 @@ public partial record ReadModel : IAsyncLogger
             this.tokenSource = null;
             await this.EnableControls(false);
 #endif
+            ECUActionArguments = null;
+            _isActive = false;
+            await this.CancelText.SetAsync("Close");
         }
     }
 
-    private async Task performRead(string path, ConnectionLease lease, ReadManager readManager, IProgress<ProgressUpdate> progress = null)
+    private async Task PerformControllerAction(ControllerManager manager)
     {
-        Stream? readContents = null;
+        bool success = false;
         try
         {
-            readContents = await readManager.Read(progress);
+            if(ECUActionArguments == null)
+            {
+                throw new ArgumentNullException(nameof(ECUActionArguments));
+            }
+            _selectedFile = ECUActionArguments.StorageFileObject as StorageFile;
+            if (_selectedFile == null)
+            {
+                throw new NullReferenceException(nameof(_selectedFile));
+            }
+            if(manager == null)
+            {
+                throw new ArgumentNullException(nameof(manager));
+            }
+            if(manager.ActionArgs.SelectedAction == ControllerActions.Undefined)
+            {
+                throw new InvalidOperationException("ControllerManager was not properly initialized with a valid action.");
+            }
+            switch(manager.ActionArgs.SelectedAction)
+            {
+                case ControllerActions.Read:
+                    if (_selectedFile != null && await manager.BeginAction() && manager.ActionArgs.ContentStream != null)
+                    {
+                        Stream writeStream = await _selectedFile.OpenStreamForWriteAsync();
+                        manager.ActionArgs.ContentStream.Position = 0;
+                        await manager.ActionArgs.ContentStream.CopyToAsync(writeStream);
+                        await writeStream.FlushAsync();
+                        await writeStream.DisposeAsync();
+                        success = true;
+                    }
+                    return;
+                case ControllerActions.Write:
+                    if (_selectedFile != null)
+                    {
+                        manager.ActionArgs.ContentStream = new MemoryStream();
+                        Stream? content = await _selectedFile.OpenStreamForReadAsync();
+                        await content.CopyToAsync(manager.ActionArgs.ContentStream);
+                        if (await manager.BeginAction())
+                        {
+                            success = true;
+                        }
+                        manager.ActionArgs.ContentStream.Dispose();
+                    }
+                    return;
+            }
         }
         catch (Exception exception)
         {
@@ -217,54 +248,50 @@ public partial record ReadModel : IAsyncLogger
             await this.AddDebugMessage(exception.ToString());
             throw;
         }
-        if (_selectedFile != null && readContents != null)
+        finally
         {
-            Stream writeStream = await _selectedFile.OpenStreamForWriteAsync();
-            await readContents.CopyToAsync(writeStream);
-            await writeStream.DisposeAsync();
+            string suffix = success == true ? "successfully." : "with errors.";
+            await this.AddUserMessage($"{this.actionText} completed {suffix}");
         }
     }
 
     [Command]
     public async ValueTask Cancel(CancellationToken ct)
     {
-        await this.AddUserMessage("Cancelling.");
-        this.tokenSource?.Cancel();
-        this.tokenSource = null;
-    }
-
-    [Command]
-    public async Task ChooseFile()
-    {
-        try
+        if (_isActive)
         {
-            await this.StartEnabled.SetAsync(false);
-            string path = await this.PromptForFileSavePath();
-            await this.Path.SetAsync(path);
-        }
-        finally
-        {
-            await this.StartEnabled.SetAsync(true);
+            await this.AddUserMessage("Cancelling.");
+            this.tokenSource?.Cancel();
+            this.tokenSource = null;
         }
     }
 
     private async Task UpdateProgress(ProgressUpdate progress)
     {
+        if (progress.UserMessage != null)
+        {
+            await AddUserMessage(progress.UserMessage);
+            return;
+        }
+        if (progress.DebugMessage != null)
+        {
+            await AddDebugMessage(progress.DebugMessage);
+            return;
+        }
 #if ANDROID
         if (Platforms.Android.DataService.IsServiceRunning())
         {
             int fixedPercentage = (int)(progress.Percentage * 100);
             Platforms.Android.DataService.UpdateProgress(fixedPercentage, $"Reading {progress.PayloadLength} bytes from 0x{progress.Address:X6}");
         }
-
 #endif
         await Invoke(async () =>
         {
-            await this.StatusUpdateActivity($"Reading {progress.PayloadLength} bytes from 0x{progress.Address:X6}");
+            await this.StatusUpdateActivity(progress.Activity);
             await this.StatusUpdateTimeRemaining($"T-{progress.TimeRemaining}");
             await this.StatusUpdatePercentDone($"{(progress.Percentage * 100.0):0.00}%");
             await this.StatusUpdateRetryCount(progress.RetryCount.ToString());
-            await this.StatusUpdateProgressBar(progress.Percentage, true);
+            await this.StatusUpdateProgressBar(progress.Percentage, progress.ProgressBarVisible);
             await this.StatusUpdateKbps($"{progress.Rate} Kbps");
         });
     }
@@ -287,23 +314,6 @@ public partial record ReadModel : IAsyncLogger
         await tcs.Task;
     }
 
-    private async Task<string> PromptForFileSavePath()
-    {
-        // Open a Save-As dialog to get the file path
-        FileSavePicker savePicker = new FileSavePicker();
-        this.platformService.PrepareChildWindow(savePicker);
-        savePicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
-        savePicker.FileTypeChoices.Add("Binary", new List<string>() { ".bin" });
-        savePicker.SuggestedFileName = "Untitled.bin";
-        StorageFile file = await savePicker.PickSaveFileAsync();
-        if (file == null)
-        {
-            return null; // TODO: change the return-type to Task<string?> in the refactoring branch.
-        }
-        _selectedFile = file;
-        return file.Name;
-    }
-
     private Task<uint> PromptForOperatingSystemId()
     {
         // TODO: OS ID dialog box
@@ -324,14 +334,13 @@ public partial record ReadModel : IAsyncLogger
     
     public async Task AddUserMessage(string message)
     {
-        _localUserMessages.Add(message);
-        await this.UserLog.SetAsync(_localUserMessages.ToArray().JoinBy("\r\n"));
+
+        await this.UserLog.Update(updater: existing => existing.Add(message), ct: CancellationToken.None);
     }
     
-    public Task AddDebugMessage(string message)
+    public async Task AddDebugMessage(string message)
     {
-        // TODO: Debug message logging
-        return Task.CompletedTask;
+
     }
 
     public async Task StatusUpdateActivity(string activity)
