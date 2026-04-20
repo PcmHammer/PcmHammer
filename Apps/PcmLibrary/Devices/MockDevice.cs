d7f8a3359ea1423e1facda734370ff4c6c33b6df
Crowbarmaster <Crowbarmasterhl2@gmail.com>
2026-04-19 20:59:45 -0400

    Ramp up the send/receive sizes of MockDevice, default size was way too time consuming.

--- Apps/PcmLibrary/Devices/MockDevice.cs
+++ Apps/PcmLibrary/Devices/MockDevice.cs
@@ -43,6 +43,8 @@ namespace PcmHacking
         /// </summary>
         public override Task<bool> Initialize()
         {
+            this.MaxSendSize = 4096 + 12;
+            this.MaxReceiveSize = 4096 + 12;
             return Task.FromResult(true);
         }
 
