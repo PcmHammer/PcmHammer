a86e89e5134744b0ec6f83c4a738c70a9611793a
Crowbarmaster <Crowbarmasterhl2@gmail.com>
2026-04-19 21:23:48 -0400

    These two objects are containers used to pass required fields into the action page.

---
+++ Apps/PcmLibrary/Misc/ControllerPageObjects.cs
@@ -0,0 +1,28 @@
+﻿using PcmHacking.ECU;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PcmHacking;
+
+public class ECUActionArguments
+{
+    public ControllerActions SelectedAction = ControllerActions.Undefined;
+    public WriteType WriteType = WriteType.None;
+    public PcmType HardwareType = PcmType.Undefined; 
+    public bool UseHighSpeed = false;
+    public bool ShowDebug = false;
+    public uint CustomKey = 0;
+    public MemoryStream ContentStream;
+    public object? StorageFileObject;
+}
+
+// Just a collection of the tasks passed to Read/WriteManager.
+public class ControllerPageObjects
+{
+    public Func<Action, Task> Invoke;
+    public Func<Task<string>> PromptForSavePath;
+    public Func<Task<UInt32>> PromptForHardwareType;
+    public Func<string, string, Task> ShowAlert;
+    public Func<string, string, Task<bool>> PromptYesOrNo;
+}
