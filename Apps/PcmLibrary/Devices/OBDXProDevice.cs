0408b3381becb91adb74379ea6f188168ce6b210
Crowbarmaster <Crowbarmasterhl2@gmail.com>
2026-05-09 23:54:31 -0400

    Fixup OBDXProDevice.IsCommandBroadcasting to utilize CompareArraysPart.

--- Apps/PcmLibrary/Devices/OBDXProDevice.cs
+++ Apps/PcmLibrary/Devices/OBDXProDevice.cs
@@ -457,7 +457,7 @@ namespace PcmHacking
         public override async Task<bool> IsCommandBroadcasting(byte command)
         {
             this.ClearMessageQueue();
-            byte[] expectedMsg = [Priority.Physical0, DeviceId.Tool, DeviceId.Pcm, command, 0x00];
+            byte[] expectedMsg = [Priority.Physical0, DeviceId.Tool, DeviceId.Pcm, command];
             try
             {
                 await this.ReadDVIPacket(200);
@@ -468,12 +468,7 @@ namespace PcmHacking
             Message incoming = await ReceiveMessage();
             if (incoming != null)
             {
-                byte[] recv = incoming.GetBytes();
-                if(recv.Length >= 5)
-                {
-                    expectedMsg[4] = recv[4];
-                }
-                if (Utility.CompareArrays(recv, expectedMsg))
+                if (Utility.CompareArraysPart(incoming.GetBytes(), expectedMsg))
                     return true;
             }
             return false;
