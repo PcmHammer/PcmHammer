5e96177a20cee6a993eb3b5f6ce08fc3afda9bdd
atshaw1994 <atshaw1994@gmail.com>
2026-06-23 20:35:00 -0400

    Add ChangeVINWindow

---
+++ Apps/UI/WPF/PCMHammer/Views/ChangeVinWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using PCMHammer.Viewmodels;
+using System.Windows;
+
+namespace PCMHammer.Views
+{
+    /// <summary>
+    /// Interaction logic for ChangeVINWindow.xaml
+    /// </summary>
+    public partial class ChangeVINWindow : Window
+    {
+        public ChangeVINWindow(ChangeVINViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+
+            // Wire up view closure handlers
+            viewModel.RequestCloseOk += () => { DialogResult = true; Close(); };
+            viewModel.RequestCloseCancel += () => { DialogResult = false; Close(); };
+        }
+    }
+}
