862fbcc57dd59b0a718426ff0fb01ca6eb551510
Aaron Shaw <atshaw1994@gmail.com>
2026-06-29 11:27:24 -0400

    Initial SettingsWindow dialog

---
+++ Apps/UI/WPF/PCMHammer/Viewmodels/BruteForceDialogBoxViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace PCMHammer.Viewmodels
+{
+    public partial class BruteForceDialogBoxViewModel : INotifyPropertyChanged
+    {
+
+
+        // INotifyPropertyChanged implementation
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged(string propertyName) => 
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
